915fd5e0f8679fa951dcb530194037dd12da5c1c
Marcus Faust <mfaust@fusionstorm.com>
2015-04-27 07:22:41 -0700

    modified:   esxi_copy_networking.py

--- esxi_copy_networking.py
+++ esxi_copy_networking.py
@@ -1,1 +1,16 @@
 __author__ = 'mxf7'
+
+from pyVim import connect,vim
+import getpass
+
+
+if __name__ == '__main__':
+
+    sourcehost = raw_input('Enter Source Host IP: ')
+    desthost = raw_input('Enter Destination Host IP: ')
+    pswd = getpass.getpass('Password: ')
+
+
+
+
+
